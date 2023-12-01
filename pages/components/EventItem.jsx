2584d9e12e3941fe59036dc625cdae2f0f5a1942
ajochymczyk <ajochymczykagh@gmail.com>
2023-12-01 20:22:33 +0100

    Dodaje opcje zapisu na zajęcia, to musze dopracować

--- pages/components/EventItem.jsx
+++ pages/components/EventItem.jsx
@@ -5,7 +5,49 @@ import './EventItem.css';
 
 const EventItem = ({ event }) => {
   const [isCancelling, setIsCancelling] = useState(false);
+  const [isSigningUp, setIsSigningUp] = useState(false);
+
   const apiBaseUrl = process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
+
+
+
+  const handleSignUpEvent = async () => {
+    try {
+      setIsSigningUp(true);
+
+      console.log('Signing up for event with ID:', event.id);
+
+      const response = await fetch(`${apiBaseUrl}/api/events/signup/${event.id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      console.log('Response:', response);
+
+      if (response.ok) {
+        console.log('User signed up successfully');
+        // Tutaj dodam kod do odświeżania informacji o wydarzeniu lub listy wydarzeń
+      } else {
+        console.error('Failed to sign up for event');
+        console.log('Error details:', response.status, response.statusText);
+      }
+    } catch (error) {
+      console.error('Error signing up for event:', error);
+      console.log('Error details:', error);
+    } finally {
+      setIsSigningUp(false);
+      console.log('Sign-up process completed');
+    }
+  };
+
+
+
+
+
+
+
 
   const handleCancelEvent = async () => {
     try {
@@ -54,13 +96,39 @@ const EventItem = ({ event }) => {
       <p>Bad Weather: {event.bad_weather ? 'Yes' : 'No'}</p>
       <p>Discount: {event.discount ? 'Yes' : 'No'}</p>
       
-      <button
+      <button className='Odwolaj'
+       style = {{    
+        backgroundColor: '#8faeca',
+        color: '#ffffff', 
+        boxShadow: '0px 4px 4px #00000040',
+        border: '#8faeca',  
+        marginLeft: '0px',
+        fontFamily: 'Source Serif Pro, serif'}}
       onClick={handleCancelEvent}
       disabled={isCancelling} 
       >
       Odwołaj
       </button>
+
+      <button className='Zapisz'
+      style = {{    
+        backgroundColor: '#8faeca',
+        color: '#ffffff', 
+        boxShadow: '0px 4px 4px #00000040',
+        border: '#8faeca',  
+        marginLeft: '10px',
+        fontFamily: 'Source Serif Pro, serif'}}
+        onClick={handleSignUpEvent}
+        disabled={isSigningUp}
+      >
+        Zapisz
+      </button>
+
+
       </div>
+
+
+      
   );
 };
 
