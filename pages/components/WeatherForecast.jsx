43caedfed0cf734a4b7c2d5e657b9842eb87cc4c
ajochymczyk <ajochymczykagh@gmail.com>
2023-12-14 11:23:16 +0100

    Dodałam nowe dane do prognozy pogody :)

--- pages/components/WeatherForecast.jsx
+++ pages/components/WeatherForecast.jsx
@@ -9,8 +9,8 @@ const PrognozaPogody = () => {
       try {
         const apiKey = '3ec5212d17e196806948efcdb3ee059e';
         const city = 'Krakow';
-        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-
+        const lang = 'pl';
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&lang=${lang}`;
         const response = await fetch(apiUrl);
         const data = await response.json();
 
@@ -29,12 +29,16 @@ const PrognozaPogody = () => {
         <div className="weather-forecast-content">
           <h2>{weather.name}</h2>
           <p>Temperatura: {Math.round(weather.main.temp - 273.15)}°C</p>
-          <p>Warunki atmosferyczne: {weather.weather[0].description}</p>
+          <p>Temperatura odczywalna: {Math.round(weather.main.feels_like - 273.15)}°C</p>
+          <p>Warunki atmosferyczne (opis): {weather.weather[0].description}</p>
           <p>Ciśnienie: {weather.main.pressure} hPa</p>
           <p>Wilgotność: {weather.main.humidity}%</p>
           <p>Wschód słońca: {new Date(weather.sys.sunrise * 1000).toLocaleTimeString()}</p>
           <p>Zachód słońca: {new Date(weather.sys.sunset * 1000).toLocaleTimeString()}</p>
-          
+          <p>Prędkość wiatru: {weather.wind.speed*3.6}km/h</p>
+          <p>Zachmurzenie: {weather.clouds.all}%</p>
+          <p>Widoczność: {weather.visibility}m</p>
+
           {weather.weather[0].icon && (
             <img
               src={`https://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
