6f6179a32dd15978eb29b6b61dbf826466418a00
ajochymczyk <ajochymczykagh@gmail.com>
2023-12-25 11:19:53 +0100

    Zajęcia o tej samej dacie wyświetlają się po klikaniu guzików wcześniejsze późniejsze

--- pages/components/AllEvents.jsx
+++ pages/components/AllEvents.jsx
@@ -1,3 +1,4 @@
+{/*
 import React from 'react';
 import EventItem from './EventItem';
 import './AllEvents.css';
@@ -14,5 +15,71 @@ const AllEvents = ({ allEvents, selectedDate }) => {
     </div>
   );
 };
+
+export default AllEvents;
+*/}
+
+
+
+import React, { useState } from 'react';
+import EventItem from './EventItem';
+import './AllEvents.css';
+import { relative } from 'path';
+
+const AllEvents = ({ allEvents, selectedDate }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  // Filtrowanie wydarzeń na podstawie wybranej daty
+  const filteredEvents = allEvents.filter(event => event.date === selectedDate);
+
+  const handlePrevClick = () => {
+    setCurrentIndex(prevIndex => (prevIndex > 0 ? prevIndex - 1 : 0));
+  };
+
+  const handleNextClick = () => {
+    setCurrentIndex(prevIndex => (prevIndex < filteredEvents.length - 1 ? prevIndex + 1 : prevIndex));
+  };
+
+  return (
+    <div className="container">
+      <div className="events-container">
+        {filteredEvents.length > 0 ? (
+          <>
+            <div className="navigation-buttons">
+              <button className="prev-button"        style = {{    
+        backgroundColor: '#8faeca',
+        color: '#ffffff', 
+        boxShadow: '0px 4px 4px #00000040',
+        border: '#8faeca',  
+        marginLeft: '0px',
+        fontFamily: 'Source Serif Pro, serif'}}
+        
+        onClick={handlePrevClick}>&lt; Wcześniejsze</button>
+              <button className="next-button"
+              
+              style = {{    
+                backgroundColor: '#8faeca',
+                color: '#ffffff', 
+                boxShadow: '0px 4px 4px #00000040',
+                border: '#8faeca',  
+                marginLeft: '0px',
+                fontFamily: 'Source Serif Pro, serif'}}
+                
+                onClick={handleNextClick}>Późniejsze zajecia &gt;</button>
+            </div>
+            <EventItem key={filteredEvents[currentIndex].id} event={filteredEvents[currentIndex]} className="event-item" />
+          </>
+        ) : (
+          <p style={{ fontSize: 'large', color: '#ffffff', position: 'relative', zIndex: '200'}}>
+          Niestety w ten dzień nie ma planowanych zajęć :( Sprawdź inną datę.
+        </p>
+        
+        )}
+      </div>
+    </div>
+  );
+};
+
+
 
 export default AllEvents;
