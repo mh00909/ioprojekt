094d8384d658913fd1b636071a37db16b4ff9c72
ajochymczyk <ajochymczykagh@gmail.com>
2024-01-03 19:10:21 +0100

    Dokumentacja AllEvents

--- pages/components/AllEvents.jsx
+++ pages/components/AllEvents.jsx
@@ -1,110 +1,101 @@
-{
-  /*
-import React from 'react';
-import EventItem from './EventItem';
-import './AllEvents.css';
-
-const AllEvents = ({ allEvents, selectedDate }) => {
-  // Filtrowanie wydarzeń na podstawie wybranej daty
-  const filteredEvents = allEvents.filter(event => event.date === selectedDate);
-
-  return (
-    <div className="container">
-      {filteredEvents.map((event) => (
-        <EventItem key={event.id} event={event} className="event-item" />
-      ))}
-    </div>
-  );
-};
-
-export default AllEvents;
-*/
-}
-
 import React, { useState } from "react";
 import EventItem from "./EventItem";
 import "./AllEvents.css";
 import { relative } from "path";
 
+
+/**
+ * Komponent `AllEvents` reprezentuje widok zawierający wszystkie wydarzenia w danym dniu.
+ * @component
+ * @param {Object[]} allEvents - Tablica zawierająca wszystkie dostępne wydarzenia.
+ * @param {string} selectedDate - Data, dla której wyświetlane są wydarzenia.
+ * @returns {JSX.Element} - Zwraca element JSX reprezentujący listę wydarzeń w danym dniu.
+ */
 const AllEvents = ({ allEvents, selectedDate }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   // Filtrowanie wydarzeń na podstawie wybranej daty
   const filteredEvents = allEvents.filter(
-    (event) => event.date === selectedDate
+      (event) => event.date === selectedDate
   );
 
+    /**
+     * Obsługuje kliknięcie przycisku poprzedniego wydarzenia.
+     */
   const handlePrevClick = () => {
     setCurrentIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : 0));
   };
 
+  /**
+   * Obsługuje kliknięcie przycisku następnego wydarzenia.
+   */
   const handleNextClick = () => {
     setCurrentIndex((prevIndex) =>
-      prevIndex < filteredEvents.length - 1 ? prevIndex + 1 : prevIndex
+        prevIndex < filteredEvents.length - 1 ? prevIndex + 1 : prevIndex
     );
   };
 
   return (
-    <div className="container">
-      <div className="events-container">
-        {filteredEvents.length > 0 ? (
-          <>
-            <div className="navigation-buttons">
-              <button
-                className="prev-button"
-                style={{
-                  backgroundColor: "#8faeca",
-                  color: "#ffffff",
-                  boxShadow: "0px 4px 4px #00000040",
-                  border: "#8faeca",
-                  marginLeft: "0px",
-                  fontFamily: "Source Serif Pro, serif",
-                }}
-                onClick={handlePrevClick}
+      <div className="container">
+        <div className="events-container">
+          {filteredEvents.length > 0 ? (
+              <>
+                <div className="navigation-buttons">
+                  <button
+                      className="prev-button"
+                      style={{
+                        backgroundColor: "#8faeca",
+                        color: "#ffffff",
+                        boxShadow: "0px 4px 4px #00000040",
+                        border: "#8faeca",
+                        marginLeft: "0px",
+                        fontFamily: "Source Serif Pro, serif",
+                      }}
+                      onClick={handlePrevClick}
+                  >
+                    &lt;{" "}
+                  </button>
+                  <button
+                      className="next-button"
+                      style={{
+                        backgroundColor: "#8faeca",
+                        color: "#ffffff",
+                        boxShadow: "0px 4px 4px #00000040",
+                        border: "#8faeca",
+                        marginLeft: "0px",
+                        fontFamily: "Source Serif Pro, serif",
+                      }}
+                      onClick={handleNextClick}
+                  >
+                    {" "}
+                    &gt;
+                  </button>
+                </div>
+                <EventItem
+                    key={filteredEvents[currentIndex].id}
+                    event={filteredEvents[currentIndex]}
+                    className="event-item"
+                />
+              </>
+          ) : (
+              <p
+                  style={{
+                    fontSize: "large",
+                    position: "relative",
+                    zIndex: "200",
+                    border: "1px solid #ccc",
+                    backgroundColor: "#ffffff",
+                    padding: "20px",
+                    borderRadius: "50px",
+                    //border: "1px solid transparent",
+                    boxShadow: "0px 4px 4px #00000040",
+                  }}
               >
-                &lt;{" "}
-              </button>
-              <button
-                className="next-button"
-                style={{
-                  backgroundColor: "#8faeca",
-                  color: "#ffffff",
-                  boxShadow: "0px 4px 4px #00000040",
-                  border: "#8faeca",
-                  marginLeft: "0px",
-                  fontFamily: "Source Serif Pro, serif",
-                }}
-                onClick={handleNextClick}
-              >
-                {" "}
-                &gt;
-              </button>
-            </div>
-            <EventItem
-              key={filteredEvents[currentIndex].id}
-              event={filteredEvents[currentIndex]}
-              className="event-item"
-            />
-          </>
-        ) : (
-          <p
-            style={{
-              fontSize: "large",
-              position: "relative",
-              zIndex: "200",
-              border: "1px solid #ccc",
-              backgroundColor: "#ffffff",
-              padding: "20px",
-              borderRadius: "50px",
-              border: "1px solid transparent",
-              boxShadow: "0px 4px 4px #00000040",
-            }}
-          >
-            Niestety w ten dzień nie ma planowanych zajęć :( Sprawdź inną datę.
-          </p>
-        )}
+                Niestety w ten dzień nie ma planowanych zajęć :( Sprawdź inną datę.
+              </p>
+          )}
+        </div>
       </div>
-    </div>
   );
 };
 
